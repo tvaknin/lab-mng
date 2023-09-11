56e102ad8a700d955ab9dfa6d2a3bc5d98f0c61b
Tslil <tslil.greenberg@harmonicinc.com>
2023-09-11 15:18:08 +0300

    add ping in server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { createSession  } = require('net-ping');
 
 const app = express();
 const port = process.env.PORT || 1990;
@@ -10,6 +11,25 @@ app.use(express.static(path.join(__dirname, 'build')));
 // Handle requests to the root route ("/")
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const session = createSession();
+
+// Define a route for the ping endpoint
+app.get('/ping', (req, res) => {
+  // Get the IP address from the "ip" query parameter, if provided
+  const ipToPing = req.query.ip || '8.8.8.8'; // Default to Google's DNS server if no IP provided
+
+  // Perform the ping
+  session.pingHost(ipToPing, (error, target) => {
+    if (!error) {
+      // If the IP is pingable, return true
+      res.json({ pingable: true });
+    } else {
+      // If the IP is not pingable, return false
+      res.json({ pingable: false });
+    }
+  });
 });
 
 // Start the server
